c983eb69d9749964a398eefc8f407d580342f5d7
Zodomo <zodomo@proton.me>
2023-11-16 00:22:29 -0600

    initial structure

--- src/GOODPERSON.sol
+++ src/GOODPERSON.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.22;
 
-contract GOODPERSON {
+import "../lib/solady/src/tokens/ERC20.sol";
+import "../lib/solady/src/auth/Ownable.sol";
 
+contract GOODPERSON is ERC20, Ownable {
+    string internal _name;
+    string internal _symbol;
+
+    uint256 public maxSupply;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 _maxSupply,
+        address _owner
+    ) {
+        _name = name_;
+        _symbol = symbol_;
+        maxSupply = _maxSupply;
+        _initializeOwner(_owner);
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+    
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
 }
