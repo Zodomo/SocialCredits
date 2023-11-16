fd4c4a63f9ee113883fe0fbd2be6da106c53a860
Zodomo <zodomo@proton.me>
2023-11-16 00:04:10 -0600

    init

---
+++ script/GOODPERSON.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.22;
+
+import {Script, console2} from "../lib/forge-std/src/Script.sol";
+
+contract GOODPERSONScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+    }
+}
---
+++ test/GOODPERSON.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.22;
+
+import {Test, console2} from "../lib/forge-std/src/Test.sol";
+import {GOODPERSON} from "../src/GOODPERSON.sol";
+
+contract CounterTest is Test {
+
+}
