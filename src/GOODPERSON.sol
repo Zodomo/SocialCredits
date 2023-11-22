167ee4c7bca931c982262803115e87c79dab2f5b
Zodomo <zodomo@proton.me>
2023-11-21 22:18:09 -0600

    removed LZ implementation and added mint/burn/reduceMaxSupply/toggleLock and general test

--- src/GOODPERSON.sol
+++ src/GOODPERSON.sol
@@ -3,58 +3,61 @@ pragma solidity ^0.8.22;
 
 import "../lib/solady/src/tokens/ERC20.sol";
 import "../lib/solady/src/auth/Ownable.sol";
-import "../lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
-import "../lib/LayerZero/contracts/interfaces/ILayerZeroReceiver.sol";
-import "../lib/solidity-bytes-utils/contracts/BytesLib.sol";
-import "../lib/LayerZero/contracts/interfaces/ILayerZeroEndpoint.sol";
 import "./libraries/Structs.sol";
 
-contract GOODPERSON is ERC20, Ownable, Initializable, ILayerZeroReceiver {
-    using BytesLib for bytes;
+contract GOODPERSON is ERC20, Ownable {
 
+    error Locked();
     error Overflow();
     error Underflow();
 
-    event LZSent(address indexed _from, address indexed _to, uint256 indexed _amount);
-    event LZReceived(address indexed _recipient, uint256 indexed _amount);
-    event LZAllocation(uint16 indexed _dstChainId, address indexed _minter, uint256 indexed _allocation);
+    event Unlocked(bool indexed _status);
     event SetAllocation(address indexed _minter, uint256 indexed _amount);
+    event SupplyReduced(uint256 indexed _maxSupply);
+
+    event OwnerMint(address indexed _to, uint256 indexed _amount);
+    event AllocationMint(address indexed _minter, address indexed _to, uint256 indexed _amount);
+    event Burn(address indexed _burner, uint256 indexed _amount);
 
     string internal _name;
     string internal _symbol;
 
     mapping(address minter => Structs.Allocation allocation) public allocations;
-    mapping(address minter => mapping(uint16 dstChainId => uint256 allocation)) public lzAllocations;
     uint256 public totalAllocated;
     uint256 public maxSupply;
-    address public lzEndpoint;
-    bool public isMainnet;
+    bool public unlocked;
 
-    modifier onlyMainnet() {
-        if (!isMainnet) revert Unauthorized();
+    modifier mintable(address _to, uint256 _amount) {
+        if (msg.sender == owner()) {
+            if (totalSupply() + totalAllocated + _amount > maxSupply) revert Overflow();
+            _;
+            emit OwnerMint(_to, _amount);
+        } else {
+            if (allocations[msg.sender].allocated - allocations[msg.sender].used < _amount) revert Unauthorized();
+            unchecked {
+                allocations[msg.sender].used += _amount;
+                totalAllocated -= _amount;
+            }
+            _;
+            emit AllocationMint(msg.sender, _to, _amount);
+        }
+    }
+
+    modifier isApprovedOrHolder(address _from, uint256 _amount) {
+        if (_from != msg.sender && allowance(_from, msg.sender) < _amount) revert Unauthorized();
         _;
     }
 
-    constructor() {}
-
-    function initialize(
+    constructor(
         string memory name_,
         string memory symbol_,
         uint256 _maxSupply,
-        address _lzEndpoint,
-        address _owner,
-        bool _isMainnet
-    ) external initializer {
+        address _owner
+    ) {
         _name = name_;
         _symbol = symbol_;
         maxSupply = _maxSupply;
-        lzEndpoint = _lzEndpoint;
         _initializeOwner(_owner);
-        isMainnet = _isMainnet;
-    }
-
-    function disableInitializers() external {
-        _disableInitializers();
     }
 
     function name() public view override returns (string memory) {
@@ -65,9 +68,15 @@ contract GOODPERSON is ERC20, Ownable, Initializable, ILayerZeroReceiver {
         return _symbol;
     }
 
-    function allocate(address _minter, uint256 _allocation) external onlyOwner onlyMainnet {
-        if (totalAllocated + _allocation > maxSupply) revert Overflow();
-        if (_allocation < allocations[_minter].used + lzAllocations[_minter][0]) revert Underflow();
+    function toggleLock() external onlyOwner {
+        bool status = !unlocked;
+        unlocked = status;
+        emit Unlocked(status);
+    }
+
+    function allocate(address _minter, uint256 _allocation) external onlyOwner {
+        if (totalAllocated + _allocation + totalSupply() > maxSupply) revert Overflow();
+        if (_allocation < allocations[_minter].used) revert Underflow();
         uint256 existingAllocation = allocations[_minter].allocated;
         if (_allocation > existingAllocation) {
             unchecked { totalAllocated += _allocation - existingAllocation; }
@@ -78,69 +87,26 @@ contract GOODPERSON is ERC20, Ownable, Initializable, ILayerZeroReceiver {
         emit SetAllocation(_minter, _allocation);
     }
 
-    function lzAllocate(
-        uint16 _dstChainId,
-        address _zroPaymentAddress,
-        uint256 _nativeFee,
-        bytes memory _adapterParams,
-        address _minter,
-        uint256 _allocation
-    ) internal {
-        if (totalAllocated + _allocation > maxSupply) revert Overflow();
-        uint256 existingAllocation = lzAllocations[_minter][_dstChainId];
-        if (_allocation <= existingAllocation) revert Underflow();
-        unchecked { totalAllocated += _allocation - existingAllocation; }
-        lzAllocations[_minter][_dstChainId] = _allocation;
-        bytes memory payload = abi.encodePacked(_minter, _allocation, true);
-        ILayerZeroEndpoint(lzEndpoint).send{ value: _nativeFee }(
-            _dstChainId,
-            abi.encodePacked(address(this), address(this)),
-            payload,
-            payable(msg.sender),
-            _zroPaymentAddress,
-            _adapterParams
-        );
-        emit LZAllocation(_dstChainId, _minter, _allocation);
+    function reduceMaxSupply(uint256 _maxSupply) external onlyOwner {
+        if (_maxSupply < totalSupply() + totalAllocated) revert Underflow();
+        maxSupply = _maxSupply;
+        emit SupplyReduced(_maxSupply);
     }
 
-    function lzSend(
-        uint16 _dstChainId,
-        address _zroPaymentAddress,
-        uint256 _nativeFee,
-        bytes memory _adapterParams,
-        address _recipient,
-        uint256 _amount
-    ) external {
-        _burn(msg.sender, _amount);
-        bytes memory payload = abi.encodePacked(_recipient, _amount, false, false);
-        ILayerZeroEndpoint(lzEndpoint).send{ value: _nativeFee }(
-            _dstChainId,
-            abi.encodePacked(address(this), address(this)),
-            payload,
-            payable(msg.sender),
-            _zroPaymentAddress,
-            _adapterParams
-        );
-        emit LZSent(msg.sender, _recipient, _amount);
+    function mint(address _to, uint256 _amount) external mintable(_to, _amount) {
+        _mint(_to, _amount);
     }
 
-    function lzReceive(
-        uint16 _srcChainId,
-        bytes calldata _srcAddress,
-        uint64,
-        bytes calldata _payload
-    ) external {
-        if (msg.sender != lzEndpoint) revert Unauthorized();
-        if (!_srcAddress.equal(abi.encodePacked(address(this), address(this)))) revert Unauthorized();
-        address recipient = _payload.slice(0, 20).toAddress(0);
-        uint256 amount = _payload.slice(20, 32).toUint256(0);
-        bool isAllocation = (_payload.slice(52, 1).toUint8(0) == 1) ? true : false;
-        bool isDeduction  = (_payload.slice(53, 1).toUint8(0) == 1) ? true : false; // TODO: Handle allocation deductions;
-        emit LZReceived(recipient, amount);
-        if (!isAllocation) {
-            _mint(recipient, amount);
-        } else {
-            // TODO: Process allocation logic
+    function burn(address _from, uint256 _amount) external isApprovedOrHolder(_from, _amount) {
+        _burn(_from, _amount);
+        unchecked { maxSupply -= _amount; }
+        emit Burn(_from, _amount);
+    }
+
+    function _beforeTokenTransfer(address _from, address _to, uint256) internal view override {
+        if (_to == address(0)) return;
+        if (_from != address(0)) {
+            if (_from != owner() && !unlocked) revert Locked();
         }
     }
 }
--- test/GOODPERSON.t.sol
+++ test/GOODPERSON.t.sol
@@ -4,6 +4,44 @@ pragma solidity ^0.8.22;
 import {Test, console2} from "../lib/forge-std/src/Test.sol";
 import {GOODPERSON} from "../src/GOODPERSON.sol";
 
-contract CounterTest is Test {
+contract GOODPERSONTest is Test {
+    GOODPERSON public token;
+    address public bobby = makeAddr("BOBBY");
 
+    function setUp() public {
+        token = new GOODPERSON("GOOD PERSON", "GOODPERSON", 1_000_000_000 ether, address(this));
+    }
+
+    function testGeneral() public {
+        token.mint(address(this), 1_000 ether);
+        require(token.maxSupply() == 1_000_000_000 ether, "maxSupply error");
+        require(token.totalSupply() == 1_000 ether, "totalSupply error");
+        require(token.balanceOf(address(this)) == 1_000 ether, "balanceOf error");
+        require(token.totalAllocated() == 0, "totalAllocated error");
+        token.allocate(bobby, 5_000 ether);
+        require(token.totalAllocated() == 5_000 ether, "totalAllocated error");
+        vm.prank(bobby);
+        token.mint(bobby, 5_000 ether);
+        require(token.maxSupply() == 1_000_000_000 ether, "maxSupply error");
+        require(token.totalSupply() == 6_000 ether, "totalSupply error");
+        require(token.balanceOf(bobby) == 5_000 ether, "balanceOf error");
+        require(token.totalAllocated() == 0, "totalAllocated error");
+        token.transfer(bobby, 100 ether);
+        require(token.balanceOf(address(this)) == 900 ether, "balanceOf error");
+        require(token.balanceOf(bobby) == 5_100 ether, "balanceOf error");
+        vm.prank(bobby);
+        vm.expectRevert(GOODPERSON.Locked.selector);
+        token.transfer(address(1), 100 ether);
+        token.toggleLock();
+        vm.prank(bobby);
+        token.transfer(address(1), 100 ether);
+        require(token.balanceOf(address(1)) == 100 ether, "balanceOf error");
+        require(token.balanceOf(bobby) == 5_000 ether, "balanceOf error");
+        vm.prank(address(1));
+        token.approve(address(this), 100 ether);
+        token.burn(address(1), 100 ether);
+        require(token.balanceOf(address(1)) == 0, "balanceOf error");
+        require(token.totalSupply() == 5_900 ether, "totalSupply error");
+        require(token.maxSupply() == 999_999_900 ether, "maxSupply error");
+    }
 }
